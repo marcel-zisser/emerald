20becabe1d0d929d65b3fdc98fc44e147e2f4405
Marcel Zisser <marcel.zisser@student.tugraz.at>
2024-12-04 17:55:26 +0100

    Added API project

---
+++ apps/emerald-api/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'emerald-api',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/emerald-api',
+};
