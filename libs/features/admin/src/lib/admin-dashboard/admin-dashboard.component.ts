c88919bdfb6024afacbbb8ead0203fe474d34cff
Marcel Zisser <marcel.zisser@student.tugraz.at>
2024-12-10 00:07:08 +0100

    Started admin dashboard

--- libs/features/admin/src/lib/admin-dashboard/admin-dashboard.component.ts
+++ libs/features/admin/src/lib/admin-dashboard/admin-dashboard.component.ts
@@ -1,11 +1,33 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatSidenav, MatSidenavContainer, MatSidenavContent } from '@angular/material/sidenav';
+import { MatListItem, MatNavList } from '@angular/material/list';
+import { MatIcon } from '@angular/material/icon';
+import { MatIconButton } from '@angular/material/button';
+import { NgOptimizedImage } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'admin-dashboard',
-  imports: [],
+  imports: [
+    MatSidenavContainer,
+    MatNavList,
+    MatListItem,
+    MatSidenavContent,
+    MatSidenav,
+    MatIcon,
+    MatIconButton,
+    NgOptimizedImage,
+    RouterOutlet
+  ],
   templateUrl: './admin-dashboard.component.html',
   styleUrl: './admin-dashboard.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
 })
-export class AdminDashboardComponent {}
+export class AdminDashboardComponent {
+  isExpanded = true;
+
+  toggleSidebar() {
+    this.isExpanded = !this.isExpanded;
+  }
+}
